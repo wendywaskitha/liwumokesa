2a900447156bcb4bd30052d8ce5d5c1dcc07db34
jatikusuma <wendywas22@gmail.com>
2025-06-26 14:05:21 +0800

    gasss lagi

--- app/Models/User.php
+++ app/Models/User.php
@@ -131,9 +131,10 @@ class User extends Authenticatable implements FilamentUser
         // return $this->isAdmin();
 
         // Opsi 2: Semua user bisa akses (untuk testing)
-        return true;
+        // return true;
 
         // Opsi 3: Berdasarkan email domain tertentu
         // return str_ends_with($this->email, '@yourdomain.com');
+        return $this->role === 'admin' || $this->isAdmin;
     }
 }
